e6423751f89e1383f3902da3975476b505e3670a
omar hassan <36304190+o3m7a7r@users.noreply.github.com>
2020-04-01 09:02:07 +0200

    .

--- Services/src/main/java/SEIIProject/Repository/DataRepository.java
+++ Services/src/main/java/SEIIProject/Repository/DataRepository.java
@@ -1,5 +1,27 @@
 package SEIIProject.Repository;
 
-public class DataRepository {
+import java.sql.*;
 
+public class DataRepository {
+	
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	
+	
+	public DataRepository()
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/name of the data base", "root", "");
+			st = con.createStatement();
+			
+			
+		}catch(Exception e) 
+		{
+			System.out.println("error : " + e);
+		}
+	}
+	
 }
--- Services/src/main/java/SEIIProject/Repository/PrintAllController.java
+++ Services/src/main/java/SEIIProject/Repository/PrintAllController.java
@@ -1,5 +1,15 @@
 package SEIIProject.Repository;
 
 public class PrintAllController {
-
+	private String query;
+	
+	
+	public PrintAllController() {
+		query = null;
+	}
+	
+	public void printAllData() 
+	{
+		
+	}
 }
